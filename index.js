125676a09aa2d470d11b7ff0d0c499ac7989981b
lukayanes <luka@summitgroupacq.com>
2026-03-08 15:45:01 -0400

    Replace empty Zestimate with variable zestimate

--- index.js
+++ index.js
@@ -64,7 +64,7 @@ export default {
         "",                                  // Motivation
         "",                                  // AskingPrice
         "",                                  // Listed
-        "",                                  // Zestimate
+        zestimate,                                  // Zestimate
         "",                              // Status
 
         "",                                  // Geolocation
